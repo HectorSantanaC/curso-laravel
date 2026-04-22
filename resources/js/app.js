0c90468c22e84d5ac89f1ab21b883e2e443776e1
Héctor Santana <hector.santana.gc@gmail.com>
2026-04-22 18:14:32 +0100

    Componentes Vue y comunicación con Laravel

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,8 @@
-import { createApp } from 'vue/dist/vue.js';
 import './bootstrap';
 
 import { createApp } from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'
+import BookList from '@/components/BookList.vue'
 
 // Crear aplicación
-
-createApp(HelloWorld).mount("#app");
+createApp(BookList).mount("#app");
